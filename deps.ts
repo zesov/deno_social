36e67f800591f61b13ffd067eef3bc19e084e7ff
63720750@qq.com <ccj@appnxt.com>
2023-08-02 15:57:49 +0800

    更新 deps.ts mysql -> @2.11.0

--- deps.ts
+++ deps.ts
@@ -21,7 +21,7 @@ export { nanoid } from "https://deno.land/x/nanoid@v3.0.0/mod.ts";
 export { AES } from "https://deno.land/x/god_crypto@v1.4.9/aes.ts";
 
 export { Logger } from "https://deno.land/x/deno_wechat@0.0.3/utils/logger.ts";
-export { Client, configLogger } from "https://deno.land/x/mysql@v2.8.0/mod.ts";
-export type { ClientConfig } from "https://deno.land/x/mysql@v2.8.0/mod.ts";
-export type { ExecuteResult } from "https://deno.land/x/mysql@v2.8.0/src/connection.ts";
-export type { TransactionProcessor } from "https://deno.land/x/mysql@v2.8.0/src/client.ts";
+export { Client, configLogger } from "https://deno.land/x/mysql@v2.11.0/mod.ts";
+export type { ClientConfig } from "https://deno.land/x/mysql@v2.11.0/mod.ts";
+export type { ExecuteResult } from "https://deno.land/x/mysql@v2.11.0/src/connection.ts";
+export type { TransactionProcessor } from "https://deno.land/x/mysql@v2.11.0/src/client.ts";
